fcd2a60a63d30d22638b0eee39c9e169e666487f
theRex <olosun.m1904582@st.futminna.edu.ng>
2026-01-30 08:30:34 +0100

    test: add getAllUsers method to AccountHelper

--- tests/helpers/accounts.ts
+++ tests/helpers/accounts.ts
@@ -8,4 +8,13 @@ export class AccountHelper {
   static getUser(simnet: Simnet, index: number) {
     return simnet.getAccounts().get(`wallet_${index}`)!;
   }
+  
+  static getAllUsers(simnet: Simnet) {
+    const accounts = simnet.getAccounts();
+    return [
+      accounts.get("wallet_1")!,
+      accounts.get("wallet_2")!,
+      accounts.get("wallet_3")!,
+    ];
+  }
 }
