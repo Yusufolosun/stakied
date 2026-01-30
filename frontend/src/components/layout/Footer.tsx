01acb7ee035dec8a50ef0b288f291b873f582d82
theRex <olosun.m1904582@st.futminna.edu.ng>
2026-01-30 09:53:55 +0100

    feat: create Footer component

---
+++ frontend/src/components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Footer: React.FC = () => {
+  return (
+    <footer className="footer">
+      <p>© 2026 Stakied Protocol. All rights reserved.</p>
+    </footer>
+  );
+};
