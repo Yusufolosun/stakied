17ee4a5fd48827bb3b21feee3c0ed5acf6d71ea5
theRex <olosun.m1904582@st.futminna.edu.ng>
2026-01-30 08:35:45 +0100

    scripts: create pool stats script

---
+++ scripts/analytics/pool-stats.ts
@@ -0,0 +1,8 @@
+import { StacksMainnet } from "@stacks/network";
+
+async function getPoolStats(maturity: number) {
+  console.log(\`Fetching pool stats for maturity \${maturity}...\`);
+  return {};
+}
+
+export { getPoolStats };
