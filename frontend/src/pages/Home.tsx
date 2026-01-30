538a4ddeca626c044c8a6bbbb0a37c4219ab7ce8
theRex <olosun.m1904582@st.futminna.edu.ng>
2026-01-30 09:58:49 +0100

    feat: create Home page

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Home: React.FC = () => {
+  return (
+    <div className="home-page">
+      <section className="hero">
+        <h1>Stakied Protocol</h1>
+        <p>Decentralized Yield Trading on Stacks</p>
+      </section>
+    </div>
+  );
+};
