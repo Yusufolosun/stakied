976677944bfcd72eace4f0158573e51a3bed0558
theRex <olosun.m1904582@st.futminna.edu.ng>
2026-01-30 08:35:37 +0100

    scripts: create mint PT/YT script

---
+++ scripts/interact/mint-pt-yt.ts
@@ -0,0 +1,7 @@
+import { StacksMainnet } from "@stacks/network";
+
+async function mintPTYT(amount: number, maturity: number) {
+  console.log(\`Minting PT/YT for maturity \${maturity}...\`);
+}
+
+export { mintPTYT };
