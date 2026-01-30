bf8c7b55cea377a5c91ccbf53796d2243f506c04
theRex <olosun.m1904582@st.futminna.edu.ng>
2026-01-30 08:35:42 +0100

    test: create error codes constants

---
+++ scripts/interact/claim-yield.ts
@@ -0,0 +1,7 @@
+import { StacksMainnet } from "@stacks/network";
+
+async function claimYield(maturity: number) {
+  console.log(\`Claiming yield for maturity \${maturity}...\`);
+}
+
+export { claimYield };
