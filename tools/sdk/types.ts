250d05b130bdc944b4d4f2dd4b0fbe7ae6f01811
theRex <olosun.m1904582@st.futminna.edu.ng>
2026-01-30 08:56:20 +0100

    sdk: add PT/YT operation types

--- tools/sdk/types.ts
+++ tools/sdk/types.ts
@@ -10,3 +10,19 @@ export interface RedeemOptions {
   senderKey: string;
   fee?: number;
 }
+
+export interface MintPtYtOptions {
+  syAmount: number;
+  maturity: number;
+  minOutput: number;
+  senderKey: string;
+  fee?: number;
+}
+
+export interface RedeemPtOptions {
+  ptAmount: number;
+  maturity: number;
+  minOutput: number;
+  senderKey: string;
+  fee?: number;
+}
