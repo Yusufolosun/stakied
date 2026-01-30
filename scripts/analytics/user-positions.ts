5f3b62342df8584a6acc239b6d974878ce03c709
theRex <olosun.m1904582@st.futminna.edu.ng>
2026-01-30 08:35:46 +0100

    scripts: create user positions script

---
+++ scripts/analytics/user-positions.ts
@@ -0,0 +1,8 @@
+import { StacksMainnet } from "@stacks/network";
+
+async function getUserPositions(address: string) {
+  console.log(\`Fetching positions for \${address}...\`);
+  return {};
+}
+
+export { getUserPositions };
