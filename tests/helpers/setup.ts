72ca7074bcf005a2d914afd48b6c655af011a372
theRex <olosun.m1904582@st.futminna.edu.ng>
2026-01-30 08:29:17 +0100

    test: create TestSetup class skeleton

---
+++ tests/helpers/setup.ts
@@ -0,0 +1,6 @@
+import { Simnet } from "@hirosystems/clarinet-sdk";
+import { Cl } from "@stacks/transactions";
+
+export class TestSetup {
+  static readonly DEPLOYER_ADDRESS = "ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM";
+}
