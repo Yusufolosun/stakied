4266f5348d978d5f4d5a22dc278015f449697425
theRex <olosun.m1904582@st.futminna.edu.ng>
2026-01-30 09:59:34 +0100

    feat: create constants file

---
+++ frontend/src/utils/constants.ts
@@ -0,0 +1,14 @@
+export const MAINNET_CONTRACT_ADDRESS = 'SP1M46W6CVGAMH3ZJD3TKMY5KCY48HWAZK0DYG193';
+export const TESTNET_CONTRACT_ADDRESS = 'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM';
+
+export const CONTRACT_NAMES = {
+  SY_TOKEN: 'sy-token',
+  PT_YT_CORE: 'pt-yt-core',
+  PT_YT_AMM: 'pt-yt-amm',
+};
+
+export const NETWORK = {
+  MAINNET: 'mainnet',
+  TESTNET: 'testnet',
+  DEVNET: 'devnet',
+} as const;
