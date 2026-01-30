d34f7bbc40f252fffbb4abfcb0cac0f4ec857e8c
theRex <olosun.m1904582@st.futminna.edu.ng>
2026-01-30 08:35:38 +0100

    scripts: create redeem PT script

---
+++ scripts/interact/redeem-pt.ts
@@ -0,0 +1,7 @@
+import { StacksMainnet } from "@stacks/network";
+
+async function redeemPT(amount: number, maturity: number) {
+  console.log(\`Redeeming \${amount} PT tokens...\`);
+}
+
+export { redeemPT };
