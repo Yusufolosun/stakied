3d88dbbd0a07b4140a01c4570f0c80aad86f79b8
theRex <olosun.m1904582@st.futminna.edu.ng>
2026-01-30 09:54:29 +0100

    feat: create ConnectWallet component skeleton

---
+++ frontend/src/components/wallet/ConnectWallet.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { showConnect } from '@stacks/connect';
+
+export const ConnectWallet: React.FC = () => {
+  return (
+    <button className="connect-btn">
+      Connect Wallet
+    </button>
+  );
+};
