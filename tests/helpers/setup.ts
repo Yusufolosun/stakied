f4230f4cb3b263d4034338b34080de2f37495918
theRex <olosun.m1904582@st.futminna.edu.ng>
2026-01-30 08:29:34 +0100

    test: add getAccounts method to TestSetup

--- tests/helpers/setup.ts
+++ tests/helpers/setup.ts
@@ -3,4 +3,12 @@ import { Cl } from "@stacks/transactions";
 
 export class TestSetup {
   static readonly DEPLOYER_ADDRESS = "ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM";
+  
+  static getAccounts(simnet: Simnet) {
+    return {
+      deployer: simnet.getAccounts().get("deployer")!,
+      wallet1: simnet.getAccounts().get("wallet_1")!,
+      wallet2: simnet.getAccounts().get("wallet_2")!,
+    };
+  }
 }
