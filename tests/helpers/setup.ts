28b240cbfe086d1865f576758a8b6ac18d04fd7e
theRex <olosun.m1904582@st.futminna.edu.ng>
2026-01-30 08:29:56 +0100

    test: add mintPTYT helper method

--- tests/helpers/setup.ts
+++ tests/helpers/setup.ts
@@ -15,4 +15,9 @@ export class TestSetup {
   static fundAccount(simnet: Simnet, address: string, amount: number) {
     return simnet.callPublicFn("sy-token", "deposit", [Cl.uint(amount)], address);
   }
+  
+  static mintPTYT(simnet: Simnet, address: string, amount: number, maturity: number) {
+    return simnet.callPublicFn("pt-yt-core", "mint-pt-yt", 
+      [Cl.uint(amount), Cl.uint(maturity)], address);
+  }
 }
