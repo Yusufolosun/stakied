4856f65692a95e9114577785daa6327f4d2b974b
Yusufolosun <olosun.m1904582@st.futminna.edu.ng>
2026-02-27 05:42:58 +0100

    test: standardize SDK import in accounts helper

--- tests/helpers/accounts.ts
+++ tests/helpers/accounts.ts
@@ -1,14 +1,14 @@
-import { Simnet } from "@hirosystems/clarinet-sdk";
+import { Simnet } from "@stacks/clarinet-sdk";
 
 export class AccountHelper {
   static getDeployer(simnet: Simnet) {
     return simnet.getAccounts().get("deployer")!;
   }
-  
+
   static getUser(simnet: Simnet, index: number) {
     return simnet.getAccounts().get(`wallet_${index}`)!;
   }
-  
+
   static getAllUsers(simnet: Simnet) {
     const accounts = simnet.getAccounts();
     return [
