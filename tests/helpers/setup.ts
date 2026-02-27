5d05abacdde8524be27c480df330cc58af71a521
Yusufolosun <olosun.m1904582@st.futminna.edu.ng>
2026-02-27 05:42:49 +0100

    test: standardize SDK import in setup helper

--- tests/helpers/setup.ts
+++ tests/helpers/setup.ts
@@ -1,9 +1,9 @@
-import { Simnet } from "@hirosystems/clarinet-sdk";
+import { Simnet } from "@stacks/clarinet-sdk";
 import { Cl } from "@stacks/transactions";
 
 export class TestSetup {
   static readonly DEPLOYER_ADDRESS = "ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM";
-  
+
   static getAccounts(simnet: Simnet) {
     return {
       deployer: simnet.getAccounts().get("deployer")!,
@@ -11,13 +11,13 @@ export class TestSetup {
       wallet2: simnet.getAccounts().get("wallet_2")!,
     };
   }
-  
+
   static fundAccount(simnet: Simnet, address: string, amount: number) {
     return simnet.callPublicFn("sy-token", "deposit", [Cl.uint(amount)], address);
   }
-  
+
   static mintPTYT(simnet: Simnet, address: string, amount: number, maturity: number) {
-    return simnet.callPublicFn("pt-yt-core", "mint-pt-yt", 
+    return simnet.callPublicFn("pt-yt-core", "mint-pt-yt",
       [Cl.uint(amount), Cl.uint(maturity)], address);
   }
 }
